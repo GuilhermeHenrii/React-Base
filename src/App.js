3d352102f53ccdef5fb7bc2f2707f82ae57aa5ac
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-11-03 18:54:45 -0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import Login from './pages/login'; // não precisa chamar explicitamente o index
+import { BrowserRouter } from 'react-router-dom'; // Chamando broserRouter aqui para ele envolver todos os soutros módulos, para facilitar o uso das rotas em toda a apicação
+
+// import Login from './pages/login';
+// não precisa chamar explicitamente o index
+
 import GlobalStyles from './styles/GlobalStyles'; // todos os componentes react começam com letras maiusculas
 import Header from './components/Header';
+import Routes from './routes'; // importando o arquivo Routes que contem as configurações de roteamento da aplicação (react-router-dom)
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Page404/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+
+// estilizando a pagina de erro404
+// usando o container global
+export default function Page404() {
+  return (
+    <Container>
+      <h1>Essa página não existe :(</h1>
+    </Container>
+  );
+}
