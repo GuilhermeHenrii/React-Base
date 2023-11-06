487757a6c7d1d40ee5cc0d482c6e82838ab4024a
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-11-06 16:06:48 -0300

    history

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'; // Chamando broserRouter aqui para ele envolver todos os soutros módulos, para facilitar o uso das rotas em toda a apicação
+import { /* BrowserRouter */ Router } from 'react-router-dom'; // Chamando broserRouter aqui para ele envolver todos os soutros módulos, para facilitar o uso das rotas em toda a apicação
 
 // import Login from './pages/login';
 // não precisa chamar explicitamente o index
 
+import history from './services/history'; // history com letra minuscula
 import GlobalStyles from './styles/GlobalStyles'; // todos os componentes react começam com letras maiusculas
 import Header from './components/Header';
 import Routes from './routes'; // importando o arquivo Routes que contem as configurações de roteamento da aplicação (react-router-dom)
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Header />
       <Routes />
       <GlobalStyles />
-    </BrowserRouter>
+    </Router>
   );
 }
 
---
+++ src/services/history.js
@@ -0,0 +1,7 @@
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+export default history;
+
+// Configurando o history para substituir o BrowserRouter
