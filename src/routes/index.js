2c84fab2c4ed3bbaf8d7fd4b205389f425f0a7bb
Guilherme Henrique <115844984+GuilhermeHenrii@users.noreply.github.com>
2023-11-25 14:45:12 -0300

    refatorando os coemtarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ export default function Routes() {
   return (
     <Switch>
       <MyRoute exact path="/" component={Login} />
-      {/* exact especifica que quer a rota exata passada para redenrizar o componente */}
+      {/* exact especifica que quer a rota exata passada para renderizar o componente */}
       <MyRoute path="*" component={Page404} />
       {/* Tudo que não estiver especificado coma uma rota válida vai cair aqui nesse erro de pagina não encontrada */}
     </Switch>
